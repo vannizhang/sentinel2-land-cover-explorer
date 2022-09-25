67cd6a7c69c86a6efa3e083c2de3ec78123f445a
Jinnan Zhang <vannizhang@gmail.com>
2022-09-25 06:37:09 -0700

    build: set up storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,12 @@
+// .storybook/preview.js
+import '../src/styles/index.css';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
---
+++ src/components/ControlPanel/ChangeCompareGraph/ChangeCompareGraph.stories.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import DivergingBarChart from '../../QuickD3Chart/DivergingBarChart/DivergingBarChart';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'Example/DivergingBarChart',
+    component: DivergingBarChart,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {},
+} as ComponentMeta<typeof DivergingBarChart>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof DivergingBarChart> = (args) => (
+    <div className=" bg-gray-800 h-48 max-w-md">
+        <DivergingBarChart {...args} />
+    </div>
+);
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+    data4Bars: [
+        {
+            key: 'Tree',
+            value: -50,
+            label: 'Tree',
+            fill: 'red',
+        },
+        {
+            key: 'Water',
+            value: 20,
+            label: 'Water',
+            fill: 'blue',
+        },
+        {
+            key: 'Built',
+            value: 50,
+            label: 'Built Area',
+            fill: 'green',
+        },
+        {
+            key: 'Flooded Veg',
+            value: 100,
+            label: 'Flooded Veg',
+            fill: 'orange',
+        },
+        {
+            key: 'Crops',
+            value: -70,
+            label: 'Crops',
+            fill: 'blue',
+        },
+        {
+            key: 'Bare Ground',
+            value: 50,
+            label: 'Bare Ground',
+            fill: 'green',
+        },
+        {
+            key: 'Snow/Ice',
+            value: 100,
+            label: 'Snow/Ice',
+            fill: 'orange',
+        },
+    ],
+    showAxis: true,
+};
