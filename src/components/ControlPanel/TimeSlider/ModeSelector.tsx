2d8f2048661e67c0388676591908827edc79281a
Jinnan Zhang <vannizhang@gmail.com>
2022-10-05 14:02:58 -0700

    feat: add Mode Selector Component

--- src/components/ControlPanel/TimeSlider/ModeSelector.tsx
+++ src/components/ControlPanel/TimeSlider/ModeSelector.tsx
@@ -1,7 +1,48 @@
+import classNames from 'classnames';
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { modeChanged } from '../../../store/Map/reducer';
+import { selectMapMode } from '../../../store/Map/selectors';
+
+const BTN_CLASSNAMES =
+    'p-1 mx-2 cursor-pointer uppercase border-custom-light-blue-80';
 
 const ModeSelector = () => {
-    return <div>ModeSelector</div>;
+    const dispatch = useDispatch();
+
+    const activeMode = useSelector(selectMapMode);
+
+    const isSwipeBtnActive = activeMode === 'swipe';
+    const isStepBtnActive = activeMode === 'step';
+
+    return (
+        <div className="flex justify-center text-xs mt-4">
+            <div
+                className={classNames(BTN_CLASSNAMES, {
+                    'opacity-50': !isSwipeBtnActive,
+                    'border-b': isSwipeBtnActive,
+                })}
+                onClick={() => {
+                    dispatch(modeChanged('swipe'));
+                }}
+            >
+                <span>swipe mode</span>
+            </div>
+
+            <div
+                className={classNames(BTN_CLASSNAMES, {
+                    'opacity-50': !isStepBtnActive,
+                    'border-b': isStepBtnActive,
+                })}
+                onClick={() => {
+                    dispatch(modeChanged('step'));
+                }}
+            >
+                <span>step mode</span>
+            </div>
+        </div>
+    );
 };
 
 export default ModeSelector;
