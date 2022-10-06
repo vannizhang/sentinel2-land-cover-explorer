e289cd44f02e2c0ecd4fa8bf5756a6ff46827094
Jinnan Zhang <vannizhang@gmail.com>
2022-10-06 07:06:56 -0700

    feat: add caret-downicon to Month Picker button

--- src/components/ControlPanel/TimeSlider/MonthPicker.tsx
+++ src/components/ControlPanel/TimeSlider/MonthPicker.tsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
+import useOnClickOutside from '../../../hooks/useOnClickOutside';
 import { sentinel2AquisitionMonthChanged } from '../../../store/Map/reducer';
 import { selectSentinel2AquisitionMonth } from '../../../store/Map/selectors';
 
@@ -28,25 +29,39 @@ const MonthPicker = () => {
 
     const containerRef = useRef<HTMLDivElement>();
 
+    useOnClickOutside(containerRef, () => {
+        setShouldShowOptions(false);
+    });
+
     return (
         <div
             className="absolute top-3"
             style={{
-                right: -15,
+                right: -25,
             }}
             ref={containerRef}
         >
             <div
-                className="border border-custom-light-blue-90 opacity-80 p-1 text-xs cursor-pointer"
+                className="border border-custom-light-blue-90 opacity-80 p-1 text-xs cursor-pointer flex items-center"
                 onClick={() => {
                     setShouldShowOptions(true);
                 }}
             >
-                <span>{MONTH_ABBR[month - 1]}</span>
+                <span className="mr-1">{MONTH_ABBR[month - 1]}</span>
+
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    height="16"
+                    width="16"
+                >
+                    <path fill="currentColor" d="M13.1 6L8 11.1 2.9 6z" />
+                    <path fill="none" d="M0 0h16v16H0z" />
+                </svg>
             </div>
 
             {shouldShowOptions && (
-                <div className="absolute bottom-0 right-0 bg-custom-background border border-custom-light-blue-50 border-b-0 text-xs">
+                <div className="absolute bottom-0 left-0 right-0 bg-custom-background border border-custom-light-blue-50 border-b-0 text-xs">
                     {MONTH_ABBR.map((monthAbbr, index) => {
                         return (
                             <div
