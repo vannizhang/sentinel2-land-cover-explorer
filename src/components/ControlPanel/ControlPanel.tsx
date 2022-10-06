aafcb28dd44a5129e299a9a6c72a0fb3488272d8
Jinnan Zhang <vannizhang@gmail.com>
2022-10-06 07:53:37 -0700

    fix:remove action bar

--- src/components/ControlPanel/ControlPanel.tsx
+++ src/components/ControlPanel/ControlPanel.tsx
@@ -51,7 +51,7 @@ const ControlPanel = () => {
 
                     <Tooltip />
 
-                    <ActionBar />
+                    {/* <ActionBar /> */}
                 </div>
             )}
 
