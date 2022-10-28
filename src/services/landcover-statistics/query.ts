f0957e0d93507544fdae3455fc180a21b5ca4ca9
Jinnan Zhang <vannizhang@gmail.com>
2022-10-27 20:38:17 -0700

    services(landscover-stats): add formatLandCoverStatsFeatures

--- src/services/landcover-statistics/query.ts
+++ src/services/landcover-statistics/query.ts
@@ -1,8 +1,10 @@
 import {
+    IFeature,
     IQueryFeaturesResponse,
     queryFeatures,
 } from '@esri/arcgis-rest-feature-service';
 import { HistoricalLandCoverData } from '../sentinel-2-10m-landcover/computeHistograms';
+import { LandCoverClassification } from '../sentinel-2-10m-landcover/rasterAttributeTable';
 
 import { LAND_COVER_STATISTICS_SERVICE_URL, FIELD_NAMES } from './config';
 
@@ -21,6 +23,13 @@ const {
     CLOUD,
     RANGE,
 } = FIELD_NAMES;
+
+type PixelCountByLandCover = Record<LandCoverClassification, number>;
+
+type PixelCountByLandCoverAndYear = {
+    year: number;
+    pixelCounts: PixelCountByLandCover;
+};
 
 export type SubRegion = {
     /**
@@ -64,7 +73,7 @@ export const getCountries = async (): Promise<string[]> => {
 };
 
 /**
- * Get list of unique sub-regions of the input country
+ * Get unique list of sub-regions of the input country
  * @param country name of the country
  */
 export const getSubRegions = async (country: string): Promise<SubRegion[]> => {
@@ -94,59 +103,7 @@ export const getHistoricalLandCoverDataByCountry = async (
     countryName: string
 ): Promise<HistoricalLandCoverData[]> => {
     try {
-        const res = (await queryFeatures({
-            url: LAND_COVER_STATISTICS_SERVICE_URL,
-            where: `${COUNTRY}='${countryName}'`,
-            groupByFieldsForStatistics: `${COUNTRY}, ${YEAR}`,
-            outStatistics: [
-                {
-                    statisticType: 'sum',
-                    onStatisticField: WATER,
-                    outStatisticFieldName: WATER,
-                },
-                {
-                    statisticType: 'sum',
-                    onStatisticField: TREE,
-                    outStatisticFieldName: TREE,
-                },
-                {
-                    statisticType: 'sum',
-                    onStatisticField: FLOODED_VEG,
-                    outStatisticFieldName: FLOODED_VEG,
-                },
-                {
-                    statisticType: 'sum',
-                    onStatisticField: CROPS,
-                    outStatisticFieldName: CROPS,
-                },
-                {
-                    statisticType: 'sum',
-                    onStatisticField: BUILT,
-                    outStatisticFieldName: BUILT,
-                },
-                {
-                    statisticType: 'sum',
-                    onStatisticField: BARE,
-                    outStatisticFieldName: BARE,
-                },
-                {
-                    statisticType: 'sum',
-                    onStatisticField: SNOW,
-                    outStatisticFieldName: SNOW,
-                },
-                {
-                    statisticType: 'sum',
-                    onStatisticField: CLOUD,
-                    outStatisticFieldName: CLOUD,
-                },
-                {
-                    statisticType: 'sum',
-                    onStatisticField: RANGE,
-                    outStatisticFieldName: RANGE,
-                },
-            ],
-        })) as IQueryFeaturesResponse;
-
+        const res = await getLandCoverStatsByCountry(countryName);
         console.log(res);
     } catch (err) {
         console.log(err);
@@ -159,30 +116,138 @@ export const getHistoricalLandCoverDataBySubRegion = async (
     subRegionISOCode: string
 ): Promise<HistoricalLandCoverData[]> => {
     try {
-        const res = (await queryFeatures({
-            url: LAND_COVER_STATISTICS_SERVICE_URL,
-            where: `${ISO_CODE}='${subRegionISOCode}'`,
-            outFields: [
-                COUNTRY,
-                NAME,
-                YEAR,
-                WATER,
-                TREE,
-                FLOODED_VEG,
-                CROPS,
-                BUILT,
-                BARE,
-                SNOW,
-                CLOUD,
-                RANGE,
-            ],
-            orderByFields: YEAR,
-        })) as IQueryFeaturesResponse;
-
+        const res = await getLandCoverStatsBySunRegionISOCode(subRegionISOCode);
         console.log(res);
     } catch (err) {
         console.log(err);
     }
 
     return null;
+};
+
+const getLandCoverStatsByCountry = async (
+    countryName: string
+): Promise<PixelCountByLandCoverAndYear[]> => {
+    const res = (await queryFeatures({
+        url: LAND_COVER_STATISTICS_SERVICE_URL,
+        where: `${COUNTRY}='${countryName}'`,
+        groupByFieldsForStatistics: `${COUNTRY}, ${YEAR}`,
+        outStatistics: [
+            {
+                statisticType: 'sum',
+                onStatisticField: WATER,
+                outStatisticFieldName: WATER,
+            },
+            {
+                statisticType: 'sum',
+                onStatisticField: TREE,
+                outStatisticFieldName: TREE,
+            },
+            {
+                statisticType: 'sum',
+                onStatisticField: FLOODED_VEG,
+                outStatisticFieldName: FLOODED_VEG,
+            },
+            {
+                statisticType: 'sum',
+                onStatisticField: CROPS,
+                outStatisticFieldName: CROPS,
+            },
+            {
+                statisticType: 'sum',
+                onStatisticField: BUILT,
+                outStatisticFieldName: BUILT,
+            },
+            {
+                statisticType: 'sum',
+                onStatisticField: BARE,
+                outStatisticFieldName: BARE,
+            },
+            {
+                statisticType: 'sum',
+                onStatisticField: SNOW,
+                outStatisticFieldName: SNOW,
+            },
+            {
+                statisticType: 'sum',
+                onStatisticField: CLOUD,
+                outStatisticFieldName: CLOUD,
+            },
+            {
+                statisticType: 'sum',
+                onStatisticField: RANGE,
+                outStatisticFieldName: RANGE,
+            },
+        ],
+    })) as IQueryFeaturesResponse;
+
+    return formatLandCoverStatsFeatures(res.features);
+};
+
+/**
+ * Query Land Cover Statistics table to get features by ISO Code
+ * @param subRegionISOCode ISO Code of sub region
+ * @returns
+ */
+const getLandCoverStatsBySunRegionISOCode = async (
+    subRegionISOCode: string
+): Promise<PixelCountByLandCoverAndYear[]> => {
+    const res = (await queryFeatures({
+        url: LAND_COVER_STATISTICS_SERVICE_URL,
+        where: `${ISO_CODE}='${subRegionISOCode}'`,
+        outFields: [
+            COUNTRY,
+            NAME,
+            YEAR,
+            WATER,
+            TREE,
+            FLOODED_VEG,
+            CROPS,
+            BUILT,
+            BARE,
+            SNOW,
+            CLOUD,
+            RANGE,
+        ],
+        orderByFields: YEAR,
+    })) as IQueryFeaturesResponse;
+
+    return formatLandCoverStatsFeatures(res.features);
+};
+
+/**
+ * Format features returned from Land Cover Stats table into Historical Land Cover Data
+ * @param features features from Land Cover Stats table
+ * @returns
+ */
+const formatLandCoverStatsFeatures = (
+    features: IFeature[]
+): PixelCountByLandCoverAndYear[] => {
+    if (!features || !features.length) {
+        return [];
+    }
+
+    const pixelCountsData: PixelCountByLandCoverAndYear[] = features.map(
+        (feature) => {
+            const { attributes } = feature;
+
+            return {
+                year: attributes[YEAR],
+                pixelCounts: {
+                    'Bare Ground': attributes[BARE] as number,
+                    'Built Area': attributes[BUILT] as number,
+                    Clouds: attributes[CLOUD] as number,
+                    Crops: attributes[CROPS] as number,
+                    'Flooded Vegetation': attributes[FLOODED_VEG] as number,
+                    'No Data': 0,
+                    Rangeland: attributes[RANGE] as number,
+                    'Snow/Ice': attributes[SNOW] as number,
+                    Trees: attributes[TREE] as number,
+                    Water: attributes[WATER] as number,
+                },
+            };
+        }
+    );
+
+    return pixelCountsData;
 };
