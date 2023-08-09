42a8fa8694ad2d4041dac6f74f9880cb79ded95a
Jinnan Zhang <vannizhang@gmail.com>
2023-08-09 16:39:16 -0700

    fix: update Error Page text

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -5,8 +5,8 @@ const ErrorPage = () => {
         <div className="flex justify-center items-center w-screen h-screen theme-background text-custom-light-blue">
             <div className="max-w-2xl">
                 <p>
-                    This app is temporarily unavailable because we are having
-                    problem fetching data from Sentinel-2 10m LandCover layer.
+                    This app is temporarily unavailable due to an issue fetching
+                    data from one of the data services.
                 </p>
             </div>
         </div>
